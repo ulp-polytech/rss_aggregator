4b29f3c09fbb192b96b046016d05309412878fe7
alencar-felipe <alencar.felipe.paiva@gmail.com>
2023-12-04 00:02:35 +0100

    workflow

--- rss_aggregator.py
+++ rss_aggregator.py
@@ -1,6 +1,8 @@
 import feedparser
 from lxml import etree
 
+keywords = ["power", "energy", "havesting", "supply", "converter"]
+
 def generate_urls(input_file):
     with open(input_file, 'r') as file:
         for url in file:
@@ -17,8 +19,9 @@ def generate_feed(urls, max_items=1000):
             if item_count >= max_items:
                 return
 
-            yield entry
-            item_count += 1
+            if any(x in entry.description for x in keywords):
+                yield entry
+                item_count += 1
 
 def write_feed(generator, output_file):
     with open(output_file, "wb") as f, etree.xmlfile(f) as xf:
@@ -26,12 +29,17 @@ def write_feed(generator, output_file):
             with xf.element("channel"):
                 for entry in generator:
                     with xf.element("item"):
-                        xf.write(etree.Element("title",
-                            text=entry.title))
-                        xf.write(etree.Element("link",
-                            text=entry.link))
-                        xf.write(etree.Element("description",
-                            text=entry.description))
+                        title = etree.Element("title")
+                        title.text = entry.title
+                        xf.write(title)
+
+                        link = etree.Element("link")
+                        link.text = entry.link
+                        xf.write(link)
+
+                        description = etree.Element("description")
+                        description.text = entry.description
+                        xf.write(description)
 
 urls = generate_urls("sources.txt")
 feed = generate_feed(urls)
